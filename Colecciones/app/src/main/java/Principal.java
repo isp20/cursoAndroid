7d97512ce105c7e7e0f95536c3d1f0b5b750063b
aleramirezsj <aleramirezsj@gmail.com>
2016-05-02 08:48:22 -0300

    Colecciones hasta el punto 6 funcionando-Agregué Principal

---
+++ Colecciones/app/src/main/java/Principal.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Ramirez on 01/05/2016.
+ */
+
+
+
+class Principal {
+    public static void main(String[] main) {
+        Set<String> conjunto = new HashSet();
+        conjunto.add("manzana");
+        conjunto.add("pera");
+        conjunto.add("fresa");
+        conjunto.add("naranja");
+        conjunto.remove("pera");
+        for(String s : conjunto) {
+            System.out.println(s);
+        }
+    }
+
+}
